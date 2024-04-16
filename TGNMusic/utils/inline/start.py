ca381c45437e25f9c9583224d875f8076759d3e8
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-04-16 19:22:23 +0530

    Update start.py

--- TGNMusic/utils/inline/start.py
+++ TGNMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=url=f"https://t.me/Idhayann"),
+            InlineKeyboardButton(text="ʙᴏʏ ʙᴇsᴛɪᴇ", url=f"https://t.me/Idhayann"),
         ],
     ]
     return buttons
